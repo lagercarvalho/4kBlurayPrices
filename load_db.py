bedfc35da5828e304788f4137175a651d35762f8
Alexander Carvalho <lagercarvalho@gmail.com>
2024-03-18 21:16:01 +0100

    main file now runs the scrapers and stores the data in db

--- load_db.py
+++ load_db.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import json
+from scrapers import cdon, kvarn, imusic
 
 with open("config.json", "r") as f:
     config = json.load(f)["database"]
@@ -18,15 +19,21 @@ cursor = conn.cursor()
 create_query = f"""
 CREATE TABLE IF NOT EXISTS movies(
     vendor varchar(32) not null,
-	title varchar(256) not null,
-	current_price real not null,
-	previous_price real not null,
-	sale real
+    title varchar(256) not null,
+    current_price real not null,
+    previous_price real not null,
+    sale real
 );
 TRUNCATE TABLE movies;
 """
 cursor.execute(create_query)
 conn.commit()
+
+cdon.scrape_cdon()
+kvarn.scrape_kvarn()
+imusic.scrape_imusic()
+
+print("CSV files loaded")
 
 for file in os.listdir("data"):
     vendor = file[:-4] if file.endswith(".csv") else None
@@ -42,6 +49,8 @@ for file in os.listdir("data"):
         cursor.execute(copy_query)
 
 conn.commit()
+
+print("Data stored in DB")
 
 cursor.close()
 conn.close()
