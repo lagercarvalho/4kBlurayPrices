c8ff14cdb517de79d114c016861e446509a7a987
Alexander Carvalho <lagercarvalho@gmail.com>
2024-03-18 21:14:23 +0100

    scrapers now get links for image and product page

--- scrapers/imusic.py
+++ scrapers/imusic.py
@@ -6,60 +6,70 @@ import re
 from tqdm import tqdm
 import json
 
-with open("../config.json", "r") as f:
-    config = json.load(f)["imusic"]
+def scrape_imusic():
+    with open("../config.json", "r") as f:
+        config = json.load(f)["imusic"]
 
-url = config["url"]
-headers = config["headers"]
+    url = config["url"]
+    headers = config["headers"]
 
-home_page = requests.get(url, headers=headers)
-home_soup = BeautifulSoup(home_page.text, "html.parser")
-form_data = (
-    home_soup.find("form", class_="navbar-form navbar-right")
-    .get_text(separator=",", strip=True)
-    .split(",")[:-1]
-)
-pages = [list(map(int, page.split("–"))) for page in form_data]
-print(f"Importing {pages[-1][-1]} movies")
+    home_page = requests.get(url, headers=headers)
+    home_soup = BeautifulSoup(home_page.text, "html.parser")
+    form_data = (
+        home_soup.find("form", class_="navbar-form navbar-right")
+        .get_text(separator=",", strip=True)
+        .split(",")[:-1]
+    )
+    pages = [list(map(int, page.split("–"))) for page in form_data]
 
-movies = {"vendor": [], "title": [], "c_price": [], "p_price": [], "sale": []}
+    movies = {"vendor": [], "title": [], "c_price": [], "p_price": [], "sale": [], "img_src":[], "list_src":[]}
 
-enum = enumerate(pages)
-if "-v" in sys.argv:
-    enum = tqdm(enum, total=len(pages), desc="Processing pages")
+    enum = enumerate(pages)
+    if "-v" in sys.argv:
+        print(f"Importing {pages[-1][-1]} movies")
+        enum = tqdm(enum, total=len(pages), desc="Processing pages")
 
-for index, page in enum:
+    for index, page in enum:
 
-    if index != 0:
-        index_page = requests.get(f"{url}?offset={page[0]-1}", headers=headers)
-        page_soup = BeautifulSoup(index_page.text, "html.parser")
-    else:
-        page_soup = home_soup
-
-    page_movies = page_soup.find_all("div", class_="list-item")
-
-    for movie in page_movies:
-        regex = r"\d+(?:,\d+)?"
-        price_text = (
-            movie.find("a", class_="price").get_text(strip=True).replace(".", "")
-        )
-        c_price = float(re.search(regex, price_text).group().replace(",", "."))
-
-        p_price_tag = movie.find("span", class_="normal-price")
-        if p_price_tag:
-            p_price_text = p_price_tag.get_text(strip=True).replace(".", "")
-            p_price = float(re.search(regex, p_price_text).group().replace(",", "."))
+        if index != 0:
+            index_page = requests.get(f"{url}?offset={page[0]-1}", headers=headers)
+            page_soup = BeautifulSoup(index_page.text, "html.parser")
         else:
-            p_price = c_price
+            page_soup = home_soup
 
-        movies["vendor"].append("imusic")
-        movies["title"].append(movie.find("a", title=True)["title"])
-        movies["c_price"].append(c_price)
-        movies["p_price"].append(p_price)
-        movies["sale"].append((p_price - c_price) / p_price)
+        page_movies = page_soup.find_all("div", class_="list-item")
+
+        for movie in page_movies:
+            regex = r"\d+(?:,\d+)?"
+            price_text = (
+                movie.find("a", class_="price").get_text(strip=True).replace(".", "")
+            )
+            c_price = float(re.search(regex, price_text).group().replace(",", "."))
+
+            p_price_tag = movie.find("span", class_="normal-price")
+            if p_price_tag:
+                p_price_text = p_price_tag.get_text(strip=True).replace(".", "")
+                p_price = float(re.search(regex, p_price_text).group().replace(",", "."))
+            else:
+                p_price = c_price
+
+            title = movie.find("a", title=True)
+            movies["title"].append(title["title"])
+            movies["list_src"].append(f'https://imusic.se{title["href"]}')
+            movies["img_src"].append(title.find("img")["src"])
+
+            movies["vendor"].append("imusic")
+            movies["c_price"].append(c_price)
+            movies["p_price"].append(p_price)
+            movies["sale"].append((p_price - c_price) / p_price)
 
 
-df = pd.DataFrame(movies)
-df.to_csv("../data/imusic.csv", index=False)
+    df = pd.DataFrame(movies)
+    df.to_csv("../data/imusic.csv", index=False)
+    
+    if "-v" in sys.argv:
+        print("Importing complete, CSV data stored")
 
-print("Importing complete, CSV data stored")
+
+if __name__ == "__main__":
+    scrape_imusic()
--- scrapers/kvarn.py
+++ scrapers/kvarn.py
@@ -5,89 +5,74 @@ import math
 from tqdm import tqdm
 import json
 
-with open("../config.json", "r") as f:
-    config = json.load(f)["kvarn"]
+def scrape_kvarn():
 
-url = config["url"]
+    with open("../config.json", "r") as f:
+        config = json.load(f)["kvarn"]
 
-page = requests.post(url, json=config["json"])
-total_movies = page.json()["result"]
-print(f"Importing {total_movies} movies")
+    url = config["url"]
 
-movies = {"vendor": [], "title": [], "c_price": [], "p_price": [], "sale": []}
+    page = requests.post(url, json=config["json"])
+    total_movies = page.json()["result"]
 
-rounds = math.ceil(total_movies / 100)
+    movies = {"vendor": [], "title": [], "c_price": [], "p_price": [], "sale": [], "img_src":[], "list_src":[]}
 
-enum = range(1, rounds + 1)
-if "-v" in sys.argv:
-    enum = tqdm(enum, desc="Processing pages")
+    rounds = math.ceil(total_movies / 100)
 
-for x in enum:
-    body = {
-        "id": 11,
-        "jsonrpc": "2.0",
-        "method": "Article.list",
-        "params": [
-            {
-                "isBuyable": True,
-                "name": "sv",
-                "pricing": True,
-                "showPricesIncludingVat": True,
-            },
-            {
-                "filters": {
-                    "/showInArticlegroups": {
-                        "containsAny": [
-                            8646739,
-                            8646743,
-                            8646779,
-                            8646877,
-                            8646745,
-                            8646789,
-                            8646747,
-                            8646775,
-                            8646749,
-                            8646751,
-                            8646803,
-                            8646753,
-                            8646755,
-                            8646757,
-                            8646759,
-                            8646761,
-                            8646801,
-                            8646763,
-                            8646777,
-                            8646765,
-                        ]
-                    }
+    enum = range(1, rounds + 1)
+    if "-v" in sys.argv:
+        print(f"Importing {total_movies} movies")
+        enum = tqdm(enum, desc="Processing pages")
+
+    for x in enum:
+        body = {
+            "id": 11,
+            "jsonrpc": "2.0",
+            "method": "Article.list",
+            "params": [
+                {
+                    "isBuyable": True,
+                    "name": "sv",
+                    "pricing": True,
+                    "showPricesIncludingVat": True,
+                    "images":True,
+                    "url":"sv",
                 },
-                "descending": True,
-                "sort": "numSold",
-                "limit": 99,
-                "offset": (x - 1) * 100,
-            },
-        ],
-    }
+                {
+                    "filters": config["json"]["params"][0],
+                    "descending": True,
+                    "sort": "numSold",
+                    "limit": 99,
+                    "offset": (x - 1) * 100,
+                },
+            ],
+        }
 
-    page = requests.post(url, json=body)
-    data = page.json()
+        page = requests.post(url, json=body)
+        data = page.json()
 
-    for movie in data["result"]:
-        pricing_info = movie["pricing"][0]
-        prev_price = pricing_info["regular"]["incVat"]["SEK"]
-        curr_price = (
-            pricing_info["specialOffer"]["incVat"]["SEK"]
-            if "specialOffer" in pricing_info
-            else prev_price
-        )
+        for movie in data["result"]:
+            pricing_info = movie["pricing"][0]
+            prev_price = pricing_info["regular"]["incVat"]["SEK"]
+            curr_price = (
+                pricing_info["specialOffer"]["incVat"]["SEK"]
+                if "specialOffer" in pricing_info
+                else prev_price
+            )
 
-        movies["vendor"].append("kvarn")
-        movies["title"].append(movie["name"]["sv"])
-        movies["c_price"].append(curr_price)
-        movies["p_price"].append(prev_price)
-        movies["sale"].append((prev_price - curr_price) / prev_price)
+            movies["img_src"].append(movie["images"][0])
+            movies["list_src"].append(movie["url"]["sv"])
+            movies["vendor"].append("kvarn")
+            movies["title"].append(movie["name"]["sv"])
+            movies["c_price"].append(curr_price)
+            movies["p_price"].append(prev_price)
+            movies["sale"].append((prev_price - curr_price) / prev_price)
 
-df = pd.DataFrame(movies)
-df.to_csv("../data/kvarn.csv", index=False)
+    df = pd.DataFrame(movies)
+    df.to_csv("../data/kvarn.csv", index=False)
 
-print("Importing complete, CSV data stored")
+    if "-v" in sys.argv:
+        print("Importing complete, CSV data stored")
+
+if __name__ == "__main__":
+    scrape_kvarn()
